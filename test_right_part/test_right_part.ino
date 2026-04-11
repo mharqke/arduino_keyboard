29ebd17e0be78e3bbaa0432134df9b5d6ab40273
mharqke <mharqke@gmail.com>
2026-04-11 16:37:22 +0300

    add sound, change | and &

--- test_right_part/test_right_part.ino
+++ test_right_part/test_right_part.ino
@@ -5,6 +5,12 @@ bool mo1_pressed = false;
 bool mo2_pressed = false;
 bool tg1_pressed = false;
 
+uint8_t sound_timer = 0;
+bool sound_val = 0;
+bool sound_flag = 0;
+uint8_t SOUND_PIN = A3;
+uint8_t sound_button_i = 4;
+uint8_t sound_button_j = 3;
 
 // #define SEARCH(arr, body, size) {\
 //   for(i = 0; i < size; i++)\
@@ -20,7 +26,7 @@ bool tg1_pressed = false;
 
 // bool is_F_key_left(uint8_t n) {
 //   register int i = 0;
-//   SEARCH(F_keys_position_list_left, (F_keys_position_list_left[i] == n), sizeof(F_keys_position_list_left)); 
+//   SEARCH(F_keys_position_list_left, (F_keys_position_list_left[i] == n), sizeof(F_keys_position_list_left));
 // };
 
 // bool is_F_key(uint8_t n, uint8_t m) {
@@ -28,9 +34,9 @@ bool tg1_pressed = false;
 //   SEARCH(F_keys_position_list, (F_keys_position_list[i][0] == n and F_keys_position_list[i][1] == m), sizeof(F_keys_position_list));
 // };
 
-bool is_fn_key(uin t16_t n) {
-  for (auto key : fn_keys_list) 
-    if (key == n) 
+bool is_fn_key(uint16_t n) {
+  for (auto key : fn_keys_list)
+    if (key == n)
       return true;
   return false;
 }
@@ -75,7 +81,7 @@ void receiveData(int bytesReceived) {
   // Serial.print("raw_data: ");
   // Serial.println(dataReceived);
 
-  if (dataReceived < 50) { // -------------------------release-------------------------
+  if (dataReceived < 50) {  // -------------------------release-------------------------
 
     if (dataReceived == mo1_button) {  // mo1 was released
       // Serial.println("left mo1 unpressed");
@@ -85,7 +91,7 @@ void receiveData(int bytesReceived) {
       memset(pressed_buttons, 0, sizeof(pressed_buttons));
     }
 
-    else if (dataReceived == mo2_button_left) { // mo2 was released
+    else if (dataReceived == mo2_button_left) {  // mo2 was released
       // Serial.print("left mo2 unpressed - ");
       // Serial.println(mo2_pressed);
       mo2_pressed = false;
@@ -94,44 +100,42 @@ void receiveData(int bytesReceived) {
       memset(pressed_buttons, 0, sizeof(pressed_buttons));
     }
 
-    else if (mo1_pressed) { // mo1 release
-      if (is_fn_key(left_keys_mo1[button_index])){
-          Consumer.release(left_keys_mo1[button_index]);
-          // Serial.println("key is fn");
-      }
-      else if (is_F_key_left(button_index)){
+    else if (mo1_pressed) {  // mo1 release
+      if (is_fn_key(left_keys_mo1[button_index])) {
+        Consumer.release(left_keys_mo1[button_index]);
+        // Serial.println("key is fn");
+      } else if (is_F_key_left(button_index)) {
         Keyboard.release(KeyboardKeycode(left_keys_mo1[button_index]));
         // Serial.println("key is f");
-      }
-      else{
+      } else {
         // Serial.print("left mo1 button: ");
         // Serial.println(left_keys_mo1[button_index]);
         Keyboard.release(left_keys_mo1[button_index]);
       }
-    } 
+    }
 
-    else if (mo2_pressed || pressed_buttons[mo2_button[0]][mo2_button[1]] == 1) { // mo2 release
-      if (is_fn_key(left_keys_mo2[button_index])){
+    else if (mo2_pressed || pressed_buttons[mo2_button[0]][mo2_button[1]] == 1) {  // mo2 release
+      if (is_fn_key(left_keys_mo2[button_index])) {
         Consumer.release(left_keys_mo2[button_index]);
       }
       // else if (tg1_pressed){
       //     Keyboard.release(KeyboardKeycode(left_keys_tg1[button_index]));
       // }
-      else{
+      else {
         // Serial.print("release mo2 button: ");
         // Serial.println(left_keys_mo2[button_index]);
         Keyboard.release(KeyboardKeycode(left_keys_mo2[button_index]));
       }
-    } 
+    }
 
-    else { // common layout release 
+    else {  // common layout release
       Keyboard.release(KeyboardKeycode(left_keys[button_index]));
     }
   }
-  
-  else if (dataReceived >= 100 and dataReceived < 150) { // -------------------------press-------------------------
-    
-    if (dataReceived == 100 + mo1_button) { // mo1 was pressed
+
+  else if (dataReceived >= 100 and dataReceived < 150) {  // -------------------------press-------------------------
+
+    if (dataReceived == 100 + mo1_button) {  // mo1 was pressed
       // Serial.println("mo1 pressed");
       mo1_pressed = true;
       Keyboard.releaseAll();
@@ -140,7 +144,7 @@ void receiveData(int bytesReceived) {
 
     }
 
-    else if (dataReceived == 100 + mo2_button_left) { // mo2 was pressed
+    else if (dataReceived == 100 + mo2_button_left) {  // mo2 was pressed
       // Serial.println("mo2 pressed");
       mo2_pressed = true;
       Keyboard.releaseAll();
@@ -148,42 +152,40 @@ void receiveData(int bytesReceived) {
       memset(pressed_buttons, 0, sizeof(pressed_buttons));
     }
 
-    else if (mo1_pressed) { // mo1 press
-      if (is_fn_key(left_keys_mo1[button_index])){
+    else if (mo1_pressed) {  // mo1 press
+      if (is_fn_key(left_keys_mo1[button_index])) {
         Consumer.press(left_keys_mo1[button_index]);
         // Serial.println("key is fn");
-      }
-      else if (is_F_key_left(button_index)){
+      } else if (is_F_key_left(button_index)) {
         Keyboard.press(KeyboardKeycode(left_keys_mo1[button_index]));
         // Serial.println("key is f");
-      }
-      else{
+      } else {
         // Serial.print("mo1 button: ");
         // Serial.println(left_keys_mo1[button_index]);
         Keyboard.press(left_keys_mo1[button_index]);
       }
-    } 
+    }
 
     else if (mo2_pressed or pressed_buttons[mo2_button[0]][mo2_button[1]] == 1) {  // mo2 press
-      if (is_fn_key(left_keys_mo2[button_index])){
+      if (is_fn_key(left_keys_mo2[button_index])) {
         Consumer.press(left_keys_mo2[button_index]);
       }
       // else if (tg1_pressed){
       //     Keyboard.press(KeyboardKeycode(left_keys_tg1[button_index]));
       // }
-      else{
+      else {
         // Serial.print("press mo2 button: ");
         // Serial.println(left_keys_mo2[button_index]);
         Keyboard.press(KeyboardKeycode(left_keys_mo2[button_index]));
       }
-    } 
+    }
 
-    else { // common layout press
+    else {  // common layout press
       Keyboard.press(KeyboardKeycode(left_keys[button_index]));
     }
 
+    sound_val = 1;
   }
-
 }
 
 void setup() {
@@ -195,6 +197,7 @@ void setup() {
   Keyboard.begin();
   Consumer.begin();
 
+  pinMode(SOUND_PIN, OUTPUT);
   for (int i = 0; i < ROWS; i++) {
     pinMode(rows[i], OUTPUT);
   }
@@ -214,75 +217,72 @@ void loop() {
 
       if (digitalRead(cols[j]) == 0) {
 
-        if (pressed_buttons[i][j] == 0) { //------------------------press------------------------
-          // Serial.print("release i - j: ");
-          // Serial.print(i);
-          // Serial.print(" - ");
-          // Serial.print(j);
-          // Serial.print("; keys[i][j]: ");
-          // Serial.println(keys[i][j]);
-          
-          if (i == mo2_button[0] and j == mo2_button[1]){ // mo2 was pressed
-              Keyboard.releaseAll();
-              Consumer.releaseAll();
-              memset(pressed_buttons, 0, sizeof(pressed_buttons));
-              // Serial.println("right mo2 press");
-          }
+        if (pressed_buttons[i][j] == 0) {  //------------------------press------------------------
+          Serial.print("release i - j: ");
+          Serial.print(i);
+          Serial.print(" - ");
+          Serial.print(j);
+          Serial.print("; keys[i][j]: ");
+          Serial.println(keys[i][j]);
 
-          else if (mo1_pressed) { // mo1 press
-            if (is_F_key(i,j)){
-              Keyboard.press(KeyboardKeycode(keys_mo1[i][j]));
-              // Serial.println("key is f");
-            }
-            else{
-            // Serial.print("press mo1: ");
-            // Serial.println(keys_mo1[i][j]);
-              Keyboard.press((keys_mo1[i][j]));
-            }
-          }
-          else { // common layout press
-            Keyboard.press(KeyboardKeycode(keys[i][j]));
-          }
-
-          pressed_buttons[i][j] = 1;
-          delay(30);
-
-        }
-
-      } 
-      else if (pressed_buttons[i][j] == 1) { //------------------------release------------------------
-          // Serial.print("press i - j: ");
-          // Serial.print(i);
-          // Serial.print(" - ");
-          // Serial.print(j);
-          // Serial.print("; keys[i][j]: ");
-          // Serial.println(keys[i][j]);
-      
-        if (i == mo2_button[0] and j == mo2_button[1]){ // mo2 was released  
+          if (i == mo2_button[0] and j == mo2_button[1]) {  // mo2 was pressed
             Keyboard.releaseAll();
             Consumer.releaseAll();
             memset(pressed_buttons, 0, sizeof(pressed_buttons));
-            // Serial.println("right mo2 release");
+            // Serial.println("right mo2 press");
+          }
+
+          else if (mo1_pressed) {  // mo1 press
+            if (sound_button_i == i and sound_button_j == j){
+              sound_flag = !sound_flag;
+            }else if (is_F_key(i, j)) {
+              Keyboard.press(KeyboardKeycode(keys_mo1[i][j]));
+              // Serial.println("key is f");
+            } else {
+              // Serial.print("press mo1: ");
+              // Serial.println(keys_mo1[i][j]);
+              Keyboard.press((keys_mo1[i][j]));
+            }
+          } else {  // common layout press
+            Keyboard.press(KeyboardKeycode(keys[i][j]));
+          }
+          sound_val = 1;
+
+          pressed_buttons[i][j] = 1;
+          delay(30);
         }
-        
-        else if (i == tg1_button[0] && j == tg1_button[1]) { // tg1 was pressed
+
+      } else if (pressed_buttons[i][j] == 1) {  //------------------------release------------------------
+        // Serial.print("press i - j: ");
+        // Serial.print(i);
+        // Serial.print(" - ");
+        // Serial.print(j);
+        // Serial.print("; keys[i][j]: ");
+        // Serial.println(keys[i][j]);
+
+        if (i == mo2_button[0] and j == mo2_button[1]) {  // mo2 was released
+          Keyboard.releaseAll();
+          Consumer.releaseAll();
+          memset(pressed_buttons, 0, sizeof(pressed_buttons));
+          // Serial.println("right mo2 release");
+        }
+
+        else if (i == tg1_button[0] && j == tg1_button[1]) {  // tg1 was pressed
           tg1_pressed = !tg1_pressed;
           Keyboard.releaseAll();
           Consumer.releaseAll();
           memset(pressed_buttons, 0, sizeof(pressed_buttons));
           // Serial.print("tg1 was pressed - ");
           // Serial.println(tg1_pressed);
-        }
-        else if (mo1_pressed) {
-          if (is_F_key(i,j)){
+        } else if (mo1_pressed) {
+          if (is_F_key(i, j)) {
             Keyboard.release(KeyboardKeycode(keys_mo1[i][j]));
-          }
-          else{
+          } else {
             Keyboard.release((keys_mo1[i][j]));
           }
         }
 
-        else { // common layout release
+        else {  // common layout release
           Keyboard.release(KeyboardKeycode(keys[i][j]));
         }
 
@@ -293,4 +293,19 @@ void loop() {
 
     digitalWrite(rows[i], 1);
   }
+
+
+  if (sound_val && sound_flag) {
+
+    digitalWrite(SOUND_PIN, sound_val);
+
+    if (sound_timer > 80) {
+      sound_val = 0;
+      sound_timer = 0;
+      digitalWrite(SOUND_PIN, sound_val);
+    } else {
+      sound_timer++;
+    }
+  }
+
 }
